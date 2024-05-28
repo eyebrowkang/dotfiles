31313375fb262bd3ff7b8ebfee95e2c17db1947b
eyebrowkang <me@ewkg.cc>
2024-05-28 22:16:11 +0800

    fix set docker

--- .config/yadm/scripts/set_docker.sh
+++ .config/yadm/scripts/set_docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# install docker
+# uninstall docker
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -14,7 +14,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # add user to docker group
-sudo groupadd docker
+# sudo groupadd docker
 sudo usermod -aG docker $USER
