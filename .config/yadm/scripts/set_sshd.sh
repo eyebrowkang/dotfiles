f66896d4cf0b3e776dea9194c802c4184b88c3f6
eyebrowkang <48169104+eyebrowkang@users.noreply.github.com>
2024-05-29 11:26:11 +0800

    improve scripts

--- .config/yadm/scripts/set_sshd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# change sshd port
-# prohibit password login
-# set ufw and failtoban
---
+++ .config/yadm/scripts/sudo_docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# uninstall docker
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get remove $pkg; done
+# Add Docker's official GPG key:
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# add user to docker group
+# groupadd docker
+usermod -aG docker $USER
